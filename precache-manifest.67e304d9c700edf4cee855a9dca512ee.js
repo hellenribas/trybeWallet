919063a8048dbc756a7c2c628a992a6e0a77bdd5
hellenribas <tchwtrabalho@gmail.com>
2022-05-04 13:55:06 -0300

    Updates

---
+++ precache-manifest.67e304d9c700edf4cee855a9dca512ee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606748c13a6f1f898e7d53475d1541a4",
+    "url": "/trybeWallet/index.html"
+  },
+  {
+    "revision": "564108245af6e3e86eef",
+    "url": "/trybeWallet/static/css/main.fc822c88.chunk.css"
+  },
+  {
+    "revision": "f2b6d8c17d88e1a7da5f",
+    "url": "/trybeWallet/static/js/2.c95cd63d.chunk.js"
+  },
+  {
+    "revision": "28b5e57e1e89ae408eee8d8f4d692c7c",
+    "url": "/trybeWallet/static/js/2.c95cd63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564108245af6e3e86eef",
+    "url": "/trybeWallet/static/js/main.605ca255.chunk.js"
+  },
+  {
+    "revision": "6b2d2c8bf7af5ff78fe4",
+    "url": "/trybeWallet/static/js/runtime-main.bbf6ad13.js"
+  },
+  {
+    "revision": "dc401a86ff3d3af7039634ae9c35f79c",
+    "url": "/trybeWallet/static/media/logo.dc401a86.svg"
+  },
+  {
+    "revision": "da5301369b9511fb12e914dd2bdbac02",
+    "url": "/trybeWallet/static/media/money-login.da530136.svg"
+  },
+  {
+    "revision": "ced1d15376a1d47eef45d41dbf69e48e",
+    "url": "/trybeWallet/static/media/travel-login.ced1d153.svg"
+  },
+  {
+    "revision": "552d6d1e0c33537d7aa62336afb92a90",
+    "url": "/trybeWallet/static/media/user-logo.552d6d1e.svg"
+  },
+  {
+    "revision": "5d8d3b97c80d0baf48f81012f41a6264",
+    "url": "/trybeWallet/static/media/wallet-logo.5d8d3b97.svg"
+  }
+]);
